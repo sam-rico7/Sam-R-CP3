6c41a62eec4730dd30523bb7fb16f1ee8ea50d26
sam-rico7 <samuel.rico@ucas-edu.net>
2024-10-04 02:56:52 +0000

    added static methods and menu

--- class_building.py
+++ class_building.py
@@ -1,52 +1,110 @@
-
-
 class Movie:
-    def __init__(self, name, year, director, cast, rating, genre):
+    def __init__(self, name, year, director, rating, genre, cast):
         self.name = name
         self.year = year
         self.director = director
-        self.cast = cast
         self.rating = rating
         self.genre = genre
-    
+        self.cast = cast
+
     def __str__(self):
-        return f"Name: {self.name}\nYear: {self.year}\nDirector: {self.director}\nCast: {self.cast}\nRating: {self.rating}"
-    
+        return f"Name: {self.name}\nYear: {self.year}\nDirector: {self.director}\nRating: {self.rating}\nGenre: {self.genre}\nCast: {', '.join(self.cast)}"
 
+    @staticmethod
+    def sort_movies_alphabetically(movies):
+        return sorted(movies, key=lambda movie: movie.name)
 
-movie1 = Movie("The Shawshank Redemption", 1994, "Frank Darabont", "R", "Drama", ["Tim Robbins", "Morgan Freeman"])
-movie2 = Movie("Pulp Fiction", 1994, "Quentin Tarantino", "R", "Crime", ["John Travolta", "Uma Thurman", "Samuel L. Jackson"])
-movie3 = Movie("The Godfather", 1972, "Francis Ford Coppola", "R", "Crime", ["Marlon Brando", "Al Pacino", "James Caan"])
-movie4 = Movie("Inception", 2010, "Christopher Nolan", "PG-13", "Sci-Fi", ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"])
-movie5 = Movie("The Matrix", 1999, "Lana Wachowski, Lilly Wachowski", "R", "Sci-Fi", ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"])
-movie6 = Movie("Forrest Gump", 1994, "Robert Zemeckis", "PG-13", "Drama", ["Tom Hanks", "Robin Wright", "Gary Sinise"])
-movie7 = Movie("The Dark Knight", 2008, "Christopher Nolan", "PG-13", "Action", ["Christian Bale", "Heath Ledger", "Aaron Eckhart"])
-movie8 = Movie("Schindler's List", 1993, "Steven Spielberg", "R", "Drama", ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"])
-movie9 = Movie("Fight Club", 1999, "David Fincher", "R", "Drama", ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"])
-movie10 = Movie("Goodfellas", 1990, "Martin Scorsese", "R", "Crime", ["Robert De Niro", "Ray Liotta", "Joe Pesci"])
-movie11 = Movie("The Silence of the Lambs", 1991, "Jonathan Demme", "R", "Thriller", ["Jodie Foster", "Anthony Hopkins", "Scott Glenn"])
-movie12 = Movie("Titanic", 1997, "James Cameron", "PG-13", "Romance", ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"])
-movie13 = Movie("The Lord of the Rings: The Fellowship of the Ring", 2001, "Peter Jackson", "PG-13", "Fantasy", ["Elijah Wood", "Ian McKellen", "Orlando Bloom"])
-movie14 = Movie("Gladiator", 2000, "Ridley Scott", "R", "Action", ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"])
-movie15 = Movie("The Green Mile", 1999, "Frank Darabont", "R", "Drama", ["Tom Hanks", "Michael Clarke Duncan", "David Morse"])
-movie16 = Movie("Saving Private Ryan", 1998, "Steven Spielberg", "R", "War", ["Tom Hanks", "Matt Damon", "Tom Sizemore"])
-movie17 = Movie("Jurassic Park", 1993, "Steven Spielberg", "PG-13", "Adventure", ["Sam Neill", "Laura Dern", "Jeff Goldblum"])
-movie18 = Movie("The Departed", 2006, "Martin Scorsese", "R", "Crime", ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"])
-movie19 = Movie("The Lion King", 1994, "Roger Allers, Rob Minkoff", "G", "Animation", ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"])
-movie20 = Movie("Eternal Sunshine of the Spotless Mind", 2004, "Michel Gondry", "R", "Romance", ["Jim Carrey", "Kate Winslet", "Kirsten Dunst"])
-movie21 = Movie("The Sixth Sense", 1999, "M. Night Shyamalan", "PG-13", "Thriller", ["Bruce Willis", "Haley Joel Osment", "Toni Collette"])
-movie22 = Movie("The Usual Suspects", 1995, "Bryan Singer", "R", "Mystery", ["Kevin Spacey", "Gabriel Byrne", "Chazz Palminteri"])
-movie23 = Movie("Memento", 2000, "Christopher Nolan", "R", "Thriller", ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano"])
-movie24 = Movie("Braveheart", 1995, "Mel Gibson", "R", "Biography", ["Mel Gibson", "Sophie Marceau", "Patrick McGoohan"])
-movie25 = Movie("The Terminator", 1984, "James Cameron", "R", "Sci-Fi", ["Arnold Schwarzenegger", "Linda Hamilton", "Michael Biehn"])
-movie26 = Movie("Back to the Future", 1985, "Robert Zemeckis", "PG", "Adventure", ["Michael J. Fox", "Christopher Lloyd", "Lea Thompson"])
-movie27 = Movie("Alien", 1979, "Ridley Scott", "R", "Horror", ["Sigourney Weaver", "Tom Skerritt", "John Hurt"])
-movie28 = Movie("The Truman Show", 1998, "Peter Weir", "PG", "Drama", ["Jim Carrey", "Laura Linney", "Noah Emmerich"])
-movie29 = Movie("The Breakfast Club", 1985, "John Hughes", "R", "Comedy", ["Molly Ringwald", "Emilio Estevez", "Judd Nelson"])
-movie30 = Movie("Dead Poets Society", 1989, "Peter Weir", "PG", "Drama", ["Robin Williams", "Robert Sean Leonard", "Ethan Hawke"])
-movie31 = Movie("Ferris Bueller's Day Off", 1986, "John Hughes", "PG-13", "Comedy", ["Matthew Broderick", "Alan Ruck", "Mia Sara"])
-movie32 = Movie("The Perks of Being a Wallflower", 2012, "Stephen Chbosky", "PG-13", "Drama", ["Logan Lerman", "Emma Watson", "Ezra Miller"])
-movie33 = Movie("Dazed and Confused", 1993, "Richard Linklater", "R", "Comedy", ["Jason London", "Wiley Wiggins", "Matthew McConaughey"])
+    @staticmethod
+    def sort_movies_chronologically(movies):
+        return sorted(movies, key=lambda movie: movie.year)
 
-    
+    @staticmethod
+    def search_by_genre(movies, genre):
+        return [movie for movie in movies if movie.genre.lower() == genre.lower()]
 
+    @staticmethod
+    def search_by_director(movies, director):
+        return [movie for movie in movies if movie.director.lower() == director.lower()]
+
+    @staticmethod
+    def search_by_cast(movies, actor):
+        return [movie for movie in movies if actor in map(str.lower, movie.cast)]
+
+def display_menu():
+    print("\n=== Movie Management Menu ===")
+    print("1. View Movies Sorted Alphabetically")
+    print("2. View Movies Sorted Chronologically")
+    print("3. Search Movies by Genre")
+    print("4. Search Movies by Director")
+    print("5. Search Movies by Cast Member")
+    print("6. Exit")
+    print("==============================")
+
+movies = [
+    Movie("The Shawshank Redemption", 1994, "Frank Darabont", "R", "Drama", ["Tim Robbins", "Morgan Freeman"]),
+    Movie("Pulp Fiction", 1994, "Quentin Tarantino", "R", "Crime", ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]),
+    Movie("The Godfather", 1972, "Francis Ford Coppola", "R", "Crime", ["Marlon Brando", "Al Pacino", "James Caan"]),
+    Movie("Inception", 2010, "Christopher Nolan", "PG-13", "Sci-Fi", ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"]),
+    Movie("The Matrix", 1999, "Lana Wachowski, Lilly Wachowski", "R", "Sci-Fi", ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"]),
+    Movie("Forrest Gump", 1994, "Robert Zemeckis", "PG-13", "Drama", ["Tom Hanks", "Robin Wright", "Gary Sinise"]),
+    Movie("The Dark Knight", 2008, "Christopher Nolan", "PG-13", "Action", ["Christian Bale", "Heath Ledger", "Aaron Eckhart"]),
+    Movie("Schindler's List", 1993, "Steven Spielberg", "R", "Drama", ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"]),
+    Movie("Fight Club", 1999, "David Fincher", "R", "Drama", ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"]),
+    Movie("Goodfellas", 1990, "Martin Scorsese", "R", "Crime", ["Robert De Niro", "Ray Liotta", "Joe Pesci"]),
+    Movie("The Silence of the Lambs", 1991, "Jonathan Demme", "R", "Thriller", ["Jodie Foster", "Anthony Hopkins", "Scott Glenn"]),
+    Movie("Titanic", 1997, "James Cameron", "PG-13", "Romance", ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"]),
+    Movie("The Lord of the Rings: The Fellowship of the Ring", 2001, "Peter Jackson", "PG-13", "Fantasy", ["Elijah Wood", "Ian McKellen", "Orlando Bloom"]),
+    Movie("Gladiator", 2000, "Ridley Scott", "R", "Action", ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"]),
+    Movie("The Green Mile", 1999, "Frank Darabont", "R", "Drama", ["Tom Hanks", "Michael Clarke Duncan", "David Morse"]),
+    Movie("Saving Private Ryan", 1998, "Steven Spielberg", "R", "War", ["Tom Hanks", "Matt Damon", "Tom Sizemore"]),
+    Movie("Jurassic Park", 1993, "Steven Spielberg", "PG-13", "Adventure", ["Sam Neill", "Laura Dern", "Jeff Goldblum"]),
+    Movie("The Departed", 2006, "Martin Scorsese", "R", "Crime", ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"]),
+    Movie("The Lion King", 1994, "Roger Allers, Rob Minkoff", "G", "Animation", ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"]),
+    Movie("Eternal Sunshine of the Spotless Mind", 2004, "Michel Gondry", "R", "Romance", ["Jim Carrey", "Kate Winslet", "Kirsten Dunst"]),
+    Movie("The Sixth Sense", 1999, "M. Night Shyamalan", "PG-13", "Thriller", ["Bruce Willis", "Haley Joel Osment", "Toni Collette"]),
+    Movie("The Usual Suspects", 1995, "Bryan Singer", "R", "Mystery", ["Kevin Spacey", "Gabriel Byrne", "Chazz Palminteri"]),
+    Movie("Memento", 2000, "Christopher Nolan", "R", "Thriller", ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano"]),
+    Movie("Braveheart", 1995, "Mel Gibson", "R", "Biography", ["Mel Gibson", "Sophie Marceau", "Patrick McGoohan"]),
+    Movie("The Terminator", 1984, "James Cameron", "R", "Sci-Fi", ["Arnold Schwarzenegger", "Linda Hamilton", "Michael Biehn"]),
+    Movie("Back to the Future", 1985, "Robert Zemeckis", "PG", "Adventure", ["Michael J. Fox", "Christopher Lloyd", "Lea Thompson"]),
+    Movie("Alien", 1979, "Ridley Scott", "R", "Horror", ["Sigourney Weaver", "Tom Skerritt", "John Hurt"]),
+    Movie("The Truman Show", 1998, "Peter Weir", "PG", "Drama", ["Jim Carrey", "Laura Linney", "Noah Emmerich"]),
+    Movie("The Breakfast Club", 1985, "John Hughes", "R", "Comedy", ["Molly Ringwald", "Emilio Estevez", "Judd Nelson"]),
+    Movie("Dead Poets Society", 1989, "Peter Weir", "PG", "Drama", ["Robin Williams", "Robert Sean Leonard", "Ethan Hawke"]),
+    Movie("Ferris Bueller's Day Off", 1986, "John Hughes", "PG-13", "Comedy", ["Matthew Broderick", "Alan Ruck", "Mia Sara"]),
+    Movie("The Perks of Being a Wallflower", 2012, "Stephen Chbosky", "PG-13", "Drama", ["Logan Lerman", "Emma Watson", "Ezra Miller"]),
+    Movie("Dazed and Confused", 1993, "Richard Linklater", "R", "Comedy", ["Jason London", "Wiley Wiggins", "Matthew McConaughey"])
+]
+
+while True:
+    display_menu()
+    choice = input("Please select an option (1-6): ")
+
+    if choice == '1':
+        print("\nMovies sorted alphabetically:")
+        for movie in Movie.sort_movies_alphabetically(movies):
+            print(movie)
+    elif choice == '2':
+        print("\nMovies sorted chronologically:")
+        for movie in Movie.sort_movies_chronologically(movies):
+            print(movie)
+    elif choice == '3':
+        genre = input("Enter the genre to search for: ")
+        print(f"\nMovies in the '{genre}' genre:")
+        for movie in Movie.search_by_genre(movies, genre):
+            print(movie)
+    elif choice == '4':
+        director = input("Enter the director's name to search for: ")
+        print(f"\nMovies directed by '{director}':")
+        for movie in Movie.search_by_director(movies, director):
+            print(movie)
+    elif choice == '5':
+        actor = input("Enter the actor's name to search for: ")
+        print(f"\nMovies with '{actor}' in the cast:")
+        for movie in Movie.search_by_cast(movies, actor):
+            print(movie)
+    elif choice == '6':
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid choice. Please try again.")
