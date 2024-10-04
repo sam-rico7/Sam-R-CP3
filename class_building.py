17e28b59d0af7ee923ef124248f0c26af9dfa9b9
sam-rico7 <samuel.rico@ucas-edu.net>
2024-10-04 23:59:06 +0000

    included functions as methods

--- class_building.py
+++ class_building.py
@@ -10,59 +10,99 @@ class Movie:
     def __str__(self):
         return f"Name: {self.name}\nYear: {self.year}\nDirector: {self.director}\nRating: {self.rating}\nGenre: {self.genre}\nCast: {', '.join(self.cast)}"
 
-def sort_movies_alpha(movies):
-    sorted_movies = []
-    while movies:
-        min_movie = movies[0]
+    def sort_movies_alpha(self, movies):
+        sorted_movies = []
+        while movies:
+            min_movie = movies[0]
+            for movie in movies:
+                if movie.name < min_movie.name:
+                    min_movie = movie
+            sorted_movies.append(min_movie)
+            movies.remove(min_movie)
+        return sorted_movies
+
+    def sort_movies_chrono(self, movies):
+        sorted_movies = []
+        while movies:
+            min_movie = movies[0]
+            for movie in movies:
+                if movie.year < min_movie.year:
+                    min_movie = movie
+            sorted_movies.append(min_movie)
+            movies.remove(min_movie)
+        return sorted_movies
+
+    def search_by_genre(self, movies, genre):
+        found_movies = []
         for movie in movies:
-            if movie.name < min_movie.name:
-                min_movie = movie
-        sorted_movies.append(min_movie)
-        movies.remove(min_movie)
-    return sorted_movies
+            if movie.genre.lower() == genre.lower():
+                found_movies.append(movie)
+        return found_movies
 
-def sort_movies_chrono(movies):
-    sorted_movies = []
-    while movies:
-        min_movie = movies[0]
+    def search_by_director(self, movies, director):
+        found_movies = []
         for movie in movies:
-            if movie.year < min_movie.year:
-                min_movie = movie
-        sorted_movies.append(min_movie)
-        movies.remove(min_movie)
-    return sorted_movies
+            if movie.director.lower() == director.lower():
+                found_movies.append(movie)
+        return found_movies
 
-def search_by_genre(movies, genre):
-    found_movies = []
-    for movie in movies:
-        if movie.genre.lower() == genre.lower():
-            found_movies.append(movie)
-    return found_movies
+    def search_by_cast(self, movies, actor):
+        found_movies = []
+        for movie in movies:
+            if actor.lower() in [member.lower() for member in movie.cast]:
+                found_movies.append(movie)
+        return found_movies
 
-def search_by_director(movies, director):
-    found_movies = []
-    for movie in movies:
-        if movie.director.lower() == director.lower():
-            found_movies.append(movie)
-    return found_movies
+    def display_menu(self):
+        print("\n=== Movie Management Menu ===")
+        print("1. View Movies Sorted Alphabetically")
+        print("2. View Movies Sorted Chronologically")
+        print("3. Search Movies by Genre")
+        print("4. Search Movies by Director")
+        print("5. Search Movies by Cast Member")
+        print("6. Exit")
+        print("==============================")
 
-def search_by_cast(movies, actor):
-    found_movies = []
-    for movie in movies:
-        if actor.lower() in [member.lower() for member in movie.cast]:
-            found_movies.append(movie)
-    return found_movies
+    def run(self, movies):
+        while True:
+            self.display_menu()
+            choice = input("Please select an option (1-6): ")
 
-def display_menu():
-    print("\n=== Movie Management Menu ===")
-    print("1. View Movies Sorted Alphabetically")
-    print("2. View Movies Sorted Chronologically")
-    print("3. Search Movies by Genre")
-    print("4. Search Movies by Director")
-    print("5. Search Movies by Cast Member")
-    print("6. Exit")
-    print("==============================")
+            if choice == '1':
+                print("\nMovies sorted alphabetically:")
+                sorted_movies = self.sort_movies_alpha(movies.copy())
+                for movie in sorted_movies:
+                    print(movie)
+            elif choice == '2':
+                print("\nMovies sorted chronologically:")
+                sorted_movies = self.sort_movies_chrono(movies.copy())
+                for movie in sorted_movies:
+                    print(movie)
+            elif choice == '3':
+                genre = input("Enter the genre to search for: ")
+                print(f"\nMovies in the '{genre}' genre:")
+                found_movies = self.search_by_genre(movies, genre)
+                for movie in found_movies:
+                    print(movie)
+            elif choice == '4':
+                director = input("Enter the director's name to search for: ")
+                print(f"\nMovies directed by '{director}':")
+                found_movies = self.search_by_director(movies, director)
+                for movie in found_movies:
+                    print(movie)
+            elif choice == '5':
+                actor = input("Enter the actor's name to search for: ")
+                print(f"\nMovies with '{actor}' in the cast:")
+                found_movies = self.search_by_cast(movies, actor)
+                for movie in found_movies:
+                    print(movie)
+            elif choice == '6':
+                print("Exiting the program.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
+# List of movies
 movies = [
     Movie("The Shawshank Redemption", 1994, "Frank Darabont", "R", "Drama", ["Tim Robbins", "Morgan Freeman"]),
     Movie("Pulp Fiction", 1994, "Quentin Tarantino", "R", "Crime", ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]),
@@ -84,55 +124,8 @@ movies = [
     Movie("The Departed", 2006, "Martin Scorsese", "R", "Crime", ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"]),
     Movie("The Lion King", 1994, "Roger Allers, Rob Minkoff", "G", "Animation", ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"]),
     Movie("Eternal Sunshine of the Spotless Mind", 2004, "Michel Gondry", "R", "Romance", ["Jim Carrey", "Kate Winslet", "Kirsten Dunst"]),
-    Movie("The Sixth Sense", 1999, "M. Night Shyamalan", "PG-13", "Thriller", ["Bruce Willis", "Haley Joel Osment", "Toni Collette"]),
-    Movie("The Usual Suspects", 1995, "Bryan Singer", "R", "Mystery", ["Kevin Spacey", "Gabriel Byrne", "Chazz Palminteri"]),
-    Movie("Memento", 2000, "Christopher Nolan", "R", "Thriller", ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano"]),
-    Movie("Braveheart", 1995, "Mel Gibson", "R", "Biography", ["Mel Gibson", "Sophie Marceau", "Patrick McGoohan"]),
-    Movie("The Terminator", 1984, "James Cameron", "R", "Sci-Fi", ["Arnold Schwarzenegger", "Linda Hamilton", "Michael Biehn"]),
-    Movie("Back to the Future", 1985, "Robert Zemeckis", "PG", "Adventure", ["Michael J. Fox", "Christopher Lloyd", "Lea Thompson"]),
-    Movie("Alien", 1979, "Ridley Scott", "R", "Horror", ["Sigourney Weaver", "Tom Skerritt", "John Hurt"]),
-    Movie("The Truman Show", 1998, "Peter Weir", "PG", "Drama", ["Jim Carrey", "Laura Linney", "Noah Emmerich"]),
-    Movie("The Breakfast Club", 1985, "John Hughes", "R", "Comedy", ["Molly Ringwald", "Emilio Estevez", "Judd Nelson"]),
-    Movie("Dead Poets Society", 1989, "Peter Weir", "PG", "Drama", ["Robin Williams", "Robert Sean Leonard", "Ethan Hawke"]),
-    Movie("Ferris Bueller's Day Off", 1986, "John Hughes", "PG-13", "Comedy", ["Matthew Broderick", "Alan Ruck", "Mia Sara"]),
-    Movie("The Perks of Being a Wallflower", 2012, "Stephen Chbosky", "PG-13", "Drama", ["Logan Lerman", "Emma Watson", "Ezra Miller"]),
-    Movie("Dazed and Confused", 1993, "Richard Linklater", "R", "Comedy", ["Jason London", "Wiley Wiggins", "Matthew McConaughey"])
 ]
 
-while True:
-    display_menu()
-    choice = input("Please select an option (1-6): ")
-
-    if choice == '1':
-        print("\nMovies sorted alphabetically:")
-        sorted_movies = sort_movies_alpha(movies.copy())
-        for movie in sorted_movies:
-            print(movie)
-    elif choice == '2':
-        print("\nMovies sorted chronologically:")
-        sorted_movies = sort_movies_chrono(movies.copy())
-        for movie in sorted_movies:
-            print(movie)
-    elif choice == '3':
-        genre = input("Enter the genre to search for: ")
-        print(f"\nMovies in the '{genre}' genre:")
-        found_movies = search_by_genre(movies, genre)
-        for movie in found_movies:
-            print(movie)
-    elif choice == '4':
-        director = input("Enter the director's name to search for: ")
-        print(f"\nMovies directed by '{director}':")
-        found_movies = search_by_director(movies, director)
-        for movie in found_movies:
-            print(movie)
-    elif choice == '5':
-        actor = input("Enter the actor's name to search for: ")
-        print(f"\nMovies with '{actor}' in the cast:")
-        found_movies = search_by_cast(movies, actor)
-        for movie in found_movies:
-            print(movie)
-    elif choice == '6':
-        print("Exiting the program.")
-        break
-    else:
-        print("Invalid choice. Please try again.")
+# Create a Movie instance to run the program
+manager = Movie("", 0, "", "", "", [])
+manager.run(movies)
