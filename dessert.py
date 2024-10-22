291d52168dbcf5872b11285bfedbaaac1766e749
sam-rico7 <samuel.rico@ucas-edu.net>
2024-10-22 11:31:20 -0600

    classes

--- dessert.py
+++ dessert.py
@@ -4,27 +4,29 @@ class Dessert_item:
     def __init__(self, name):
         self.name = name
 
-    
-
-
-
-
 class Candy(Dessert_item):
-    candy_weight: float = 0.0
-    price_per_pound: float = 0.0
+    def __init__(self, name: str = '', candy_weight: float = 0.0, price_per_pound: float = 0.0):
+        super().__init__(name)
+        self.candy_weight = candy_weight
+        self.price_per_pound = price_per_pound
 
 
 class Cookie(Dessert_item):
-    cookie_quantity: int = 0
-    price_per_dozen: float = 0.0
+    def __init__(self, name: str = '', cookie_quantity: int = 0, price_per_dozen: float = 0.0):
+        super().__init__(name)
+        self.cookie_quantity = cookie_quantity
+        self.price_per_dozen = price_per_dozen
 
 
 class Icecream(Dessert_item):
-    scoop_count: int = 0
-    price_per_scoop: float = 0.0
+    def __init__(self, name: str = '', scoop_count: int = 0, price_per_scoop: float = 0.0):
+        super().__init__(name)
+        self.scoop_count = scoop_count
+        self.price_per_scoop = price_per_scoop
 
 class Sundae(Icecream):
-    scoop_count: int = 0
-    price_per_scoop: float = 0.0
-    topping_name: str = ''
-    topping_price: float = 0.0
+    def __init__(self, name: str = '', scoop_count: int = 0, price_per_scoop: float = 0.0, 
+                 topping_name: str = '', topping_price: float = 0.0):
+        super().__init__(name, scoop_count, price_per_scoop)
+        self.topping_name = topping_name
+        self.topping_price = topping_price
