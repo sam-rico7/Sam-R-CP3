39094aec1a5f2cd1ae7202b47963f63be7cda986
sam-rico7 <samuel.rico@ucas-edu.net>
2024-10-28 11:38:03 -0600

    total number of items

--- Dessert/dessert.py
+++ Dessert/dessert.py
@@ -2,6 +2,8 @@
 class DessertItem:
     def __init__(self, name):
         self.name = name
+    def __str__(self):
+        return self.name
 
 class Candy(DessertItem):
     def __init__(self, name: str = '', candy_weight: float = 0.0, price_per_pound: float = 0.0):
@@ -27,3 +29,27 @@ class Sundae(IceCream):
         super().__init__(name, scoop_count, price_per_scoop)
         self.topping_name = topping_name
         self.topping_price = topping_price
+
+class Order:
+    def __init__(self):
+        self.order = []
+    def add(self, item):
+        self.order.append(item)
+    def __len__(self):
+        size = len(self.order)
+        return size
+
+def main():
+    orderone = Order()
+    orderone.add(Candy("Candy Corn", 1.5, .25))
+    orderone.add(Candy("Gummy Bears", .25, .35))
+    orderone.add(Cookie("Chocolate Chip", 6, 3.99))
+    orderone.add(IceCream("Pistachio", 2, .79))
+    orderone.add(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
+    orderone.add(Cookie("Oatmeal Raisin", 2, 3.45))
+    for item in orderone.order:
+        print(item)
+    print(f"Total number of items in order: {len(orderone)}")
+
+main()
+
