6f97f41759d28b34b1f3225a2e353ec950c180d9
sam-rico7 <samuel.rico@ucas-edu.net>
2024-10-30 11:06:17 -0600

    y

--- contacts.py
+++ contacts.py
@@ -1,7 +1,25 @@
+class Information:
+    def __init__(self):
+        self.first_name = input('Enter their first name: ')
+        self.last_name = input('Enter their last name: ')
+        self.personal_phone = input('Enter their personal phone number: ')
+        self.personal_email = input('Enter their personal email address: ')
+        self.work_phone = input('Enter their work phone number: ')
+        self.work_email = input('Enter their work email address: ')
+        self.title = input('Enter their work title: ')
+
+    def display_info(self):
+        print(f'\n{self.first_name} {self.last_name}')
+        print(f'Personal phone number: {self.personal_phone}')
+        print(f'Personal email address: {self.personal_email}')
+        print(f'Work title: {self.title}')
+        print(f'Work phone number: {self.work_phone}')
+        print(f'Work email address: {self.work_email}')
+
 
 class Contacts:
     def __init__(self):
-        self.view = 'quit'
+        self.view = 'list'
         self.contact_list = []
         self.choice = None
         self.index = None
@@ -18,15 +36,53 @@ class Contacts:
             elif self.view == 'quit':
                 print('\nClosing the contact list...\n')
                 break
-        
-    def show_list(self):
-        pass
 
-    def show_info(self):
-        pass
+    def show_list(self):
+        print()
+        if len(self.contact_list) == 0:
+            self.choice = input('(A)dd a new contact \n(Q)uit \n> ').lower()
+        else:
+            for index, contact in enumerate(self.contact_list):
+                print(f"{index + 1}) {contact.first_name} {contact.last_name}")
+            self.choice = input('\n(#) Select a name \n(A)dd a new contact\n(Q)uit \n> ').lower()
+        self.handle_choice()
+
+    def handle_choice(self):
+        if self.choice == 'q':
+            self.view = 'quit'
+        elif self.choice == 'a' and self.view == 'list':
+            self.view = 'add'
+        elif self.choice.isnumeric() and self.view == 'list':
+            index = int(self.choice) - 1
+            if 0 <= index < len(self.contact_list):
+                self.index = index
+                self.view = 'info'
+
+    def __add__(self, new_contact):
+        self.contact_list.append(new_contact)
 
     def add_contact(self):
-        pass
+        self + Information()
+        self.view = 'list'
 
-contacts = Contacts()
-contacts.display()
+    def show_info(self):
+        self.contact_list[self.index].display_info()
+        self.choice = input('\n(C)ontact List \n(P)revious contact \n(N)ext contact \n(Q)uit \n> ').lower()
+        self.handle_choice()
+
+    def display_info(self):
+        self.contact_list[self.index].display_info()
+
+    def handle_next_previous(self):
+        if self.choice == 'n' and self.view == 'info':
+            self.index = self.index + 1 if self.index + 1 < len(self.contact_list) else 0
+        elif self.choice == 'p' and self.view == 'info':
+            self.index = self.index - 1 if self.index - 1 >= 0 else len(self.contact_list) - 1
+        elif self.choice == 'c' and self.view == 'info':
+            self.view = 'list'
+
+
+# Main Program Execution
+if __name__ == "__main__":
+    contacts = Contacts()
+    contacts.display()
