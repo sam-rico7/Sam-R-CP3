ed56d2b37f3f5c212863732b4f2ace4873094fb6
sam-rico7 <samuel.rico@ucas-edu.net>
2024-12-04 11:49:13 -0700

    notes

---
+++ kivy_test.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class HelloWorldApp(App):
+    def build(self):
+        return Builder.load_file('main.kv')
+    
+HelloWorldApp().run()
+
